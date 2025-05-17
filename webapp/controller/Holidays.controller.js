bbdfe3a577df36c326917962982d525b8c1cfa23
martincollak <martin@collak.cz>
2025-05-17 17:20:25 +0200

    svátky app

---
+++ webapp/controller/Holidays.controller.js
@@ -0,0 +1,67 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment",
+    "sap/ui/core/routing/History",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, MessageToast, Fragment, History, JSONModel, formatter, Filter, FilterOperator) {
+        "use strict";
+
+        return Controller.extend("com.collak.home.central.central.controller.Holidays", {
+            formatter: formatter,
+
+            onInit: function () {
+
+                this.oMyAvatar = this.oView.byId("avatarId");
+                this._oPopover = Fragment.load({
+                    id: this.oView.getId(),
+                    name: "com.collak.home.central.central.view.fragment.Popover",
+                    controller: this
+                }).then(function (oPopover) {
+                    this.oView.addDependent(oPopover);
+                    this._oPopover = oPopover;
+                }.bind(this));
+
+                //ShellBar navigation
+                var oViewModel = new sap.ui.model.json.JSONModel({
+                    shellTitle: "Objednávky"
+                });
+                this.getView().setModel(oViewModel, "viewModel");
+
+            },
+            handleHomeIconPress: function (oEvent) {
+                const oHistory = History.getInstance();
+                const sPreviousHash = oHistory.getPreviousHash();
+
+                if (sPreviousHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    const oRouter = this.getOwnerComponent().getRouter();
+                    oRouter.navTo("RouteHome", {}, true);
+                }
+
+            },
+
+            handleAvatarPress: function (oEvent) {
+                var oEventSource = oEvent.getSource(),
+                    bActive = this.oMyAvatar.getActive();
+                this.oMyAvatar.setActive(!bActive);
+
+                if (bActive) {
+                    this._oPopover.close();
+                } else {
+                    this._oPopover.openBy(oEventSource);
+                }
+            },
+            onPopoverClose: function () {
+                this.oMyAvatar.setActive(false);
+            }
+        });
+    });
